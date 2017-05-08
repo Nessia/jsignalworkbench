5c363e5390044cae8e67d1c952f238c3a140f417
Vanesa Graíño Pazos <vanesa.graino@usc.es>
2017-05-08 17:50:50 +0200

    Cerrados inputstreams que quedaban abiertos

--- src/es/usc/gsi/trace/importer/Perfil/auxiliares/Serializador.java
+++ src/es/usc/gsi/trace/importer/Perfil/auxiliares/Serializador.java
@@ -40,41 +40,17 @@ public class Serializador {
     public PTBMInterface cargaPTBM(String path, String archivo, boolean es_jar) {
         if (path == null) {
             return cargaPTBM(archivo);
-        } else {
-            try {
-                JarInputStream jar = new JarInputStream(new FileInputStream(
-                        path
-                        /*"Herramienta_Monitorizacion.jar"*/));
-                JarEntry entry = jar.getNextJarEntry();
-                while (!entry.getName().endsWith(archivo)) {
-                    entry = jar.getNextJarEntry();
-                }
-
-                JarFile jf = new JarFile(path
-                             /*"Herramienta_Monitorizacion.jar"*/);
-                ObjectInputStream in = new ObjectInputStream(jf.getInputStream(
-                        entry));
-                PTBMInterface ptbm = (PTBMInterface) in.readObject();
-                in.close();
-                return ptbm;
-
-            } catch (Exception e) {
-                e.printStackTrace();
-                return null;
-            }
         }
-    }
-
-    public PTBMInterface cargaPTBM(String archivo, boolean es_jar) {
+        JarInputStream jar = null;
+        JarFile jf = null;
         try {
-            JarInputStream jar = new JarInputStream(new FileInputStream(
-                    "Herramienta_Monitorizacion.jar"));
+            jar = new JarInputStream(new FileInputStream( path /*"Herramienta_Monitorizacion.jar"*/));
             JarEntry entry = jar.getNextJarEntry();
             while (!entry.getName().endsWith(archivo)) {
                 entry = jar.getNextJarEntry();
             }
 
-            JarFile jf = new JarFile("Herramienta_Monitorizacion.jar");
+            jf = new JarFile(path/*"Herramienta_Monitorizacion.jar"*/);
             ObjectInputStream in = new ObjectInputStream(jf.getInputStream(
                     entry));
             PTBMInterface ptbm = (PTBMInterface) in.readObject();
@@ -84,6 +60,47 @@ public class Serializador {
         } catch (Exception e) {
             e.printStackTrace();
             return null;
+        } finally {
+            try {
+                if(jar != null){ jar.close();}
+            } catch (IOException e) {}
+
+            try {
+                if(jf != null){ jf.close();}
+            } catch (IOException e) {}
+        }
+
+    }
+
+    public PTBMInterface cargaPTBM(String archivo, boolean es_jar) {
+        JarInputStream jar = null;
+       JarFile jf = null;
+        try {
+            jar = new JarInputStream(new FileInputStream(
+                    "Herramienta_Monitorizacion.jar"));
+            JarEntry entry = jar.getNextJarEntry();
+            while (!entry.getName().endsWith(archivo)) {
+                entry = jar.getNextJarEntry();
+            }
+
+            jf = new JarFile("Herramienta_Monitorizacion.jar");
+            ObjectInputStream in = new ObjectInputStream(jf.getInputStream(
+                    entry));
+            PTBMInterface ptbm = (PTBMInterface) in.readObject();
+            in.close();
+            return ptbm;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            try {
+                if(jar != null){ jar.close();}
+            } catch (IOException e) {}
+
+            try {
+                if(jf != null){ jf.close();}
+            } catch (IOException e) {}
         }
     }
 
