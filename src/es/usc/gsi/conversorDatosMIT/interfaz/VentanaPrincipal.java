6c4b48ef7e53e77e18386ab5362f26508fec20eb
Vanesa Graíño Pazos <vanesa.graino@usc.es>
2017-05-09 18:06:24 +0200

    Cambios sugeridos por Sonar. Logger + atributo borrado

--- src/es/usc/gsi/conversorDatosMIT/interfaz/VentanaPrincipal.java
+++ src/es/usc/gsi/conversorDatosMIT/interfaz/VentanaPrincipal.java
@@ -4,6 +4,8 @@ package es.usc.gsi.conversorDatosMIT.interfaz;
 
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.swing.JFrame;
 
@@ -13,34 +15,33 @@ public class VentanaPrincipal extends JFrame {
      *
      */
     private static final long serialVersionUID = 5357930700642598451L;
+    private static final Logger LOGGER = Logger.getLogger(VentanaPrincipal.class.getName());
+    private static final int ANCHOVENTANA = 670;
+    private static final int ALTOVENTANA = 550;
 
     private ControladorInterfaz controlInterfaz = ControladorInterfaz.getControlador();
     private BarraMenuPrincipal barraMenu = new BarraMenuPrincipal();
-    private PanelPrincipal panelPrincipal; // Contenedor de todos los demas paneles.
-    private final int ANCHOVENTANA = 670;
-    private final int ALTOVENTANA = 550;
+    //private PanelPrincipal panelPrincipal; // Contenedor de todos los demas paneles.
+
 
     public VentanaPrincipal() {
         super("Conversor MITDB");
         try {
             jbInit();
         } catch (Exception e) {
-            System.out.println(panelPrincipal);
-            e.printStackTrace();
+            LOGGER.log(Level.WARNING, e.getMessage(), e);
         }
     }
 
     private void jbInit() throws Exception {
-
         this.addWindowListener(new EventoVentana());
-
         this.setSize(ANCHOVENTANA, ALTOVENTANA);
 //    this.setResizable(false);
 
         // Hay que inicializarlo despues de haberle dado un tamanho a la ventana
-        // o al contenedor, ya que el panelPrincipal ocupa todo
+        // o al contenedor, ya que el panelPrincipal ocupa t.odo
         // el espacio de su contenedor.
-        panelPrincipal = controlInterfaz.getPanelPrincipal();
+        PanelPrincipal panelPrincipal = controlInterfaz.getPanelPrincipal();
 
         this.getContentPane().add(panelPrincipal);
         // Prueba de la clase Paciente
@@ -48,13 +49,11 @@ public class VentanaPrincipal extends JFrame {
         /* Paciente paci=new Paciente(null);
          panelPrincipal.add (paci, new XYConstraints(30, 30, 500, 500) ); */
 
-
         this.setJMenuBar(barraMenu);
-
     }
 
     // InnerClass: Eventos de Ventana
-    // LLAMAR A METODOS DEL CONTROLADOR PARA COMPROBAR QUE TODO ESTA CORRECTO
+    // LLAMAR A METODOS DEL CONTROLADOR PARA COMPROBAR QUE T.ODO ESTA CORRECTO
     // ANTES DE CERRAR LA APLICACION
     class EventoVentana extends WindowAdapter {
 
