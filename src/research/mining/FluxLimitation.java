ac2de369e16b7bcc04f0c7cfbf294804d22fcbda
Vanesa Graíño Pazos <vanesa.graino@usc.es>
2017-05-18 12:38:37 +0200

    Refactoring para reducir codigo repetido

--- src/research/mining/FluxLimitation.java
+++ src/research/mining/FluxLimitation.java
@@ -6,6 +6,7 @@ import net.javahispano.jsignalwb.jsignalmonitor.TimeRepresentation;
 
 
 public class FluxLimitation extends TemporalEvent {
+
     //Energia de la senhal normalizada por la duracion del episodio
     private float energy;
     //Buscar 10 segundos de senhal de flujo respiratorio sin limitacion justo antes
@@ -17,11 +18,9 @@ public class FluxLimitation extends TemporalEvent {
     //si es hipoapnea o apnea
     private Type type;
     //lista de limitaciones de movimiento abdominal
-    private List<AbdominalMovementLimutation> abdominalLimitations =
-            new LinkedList<AbdominalMovementLimutation>();
+    private List<FluxLimitation> abdominalLimitations = new LinkedList<FluxLimitation>();
     //lista de limitaciones de movimiento toracico
-    private List<ThoracicMovementLimutation> thoracicLimitations =
-            new LinkedList<ThoracicMovementLimutation>();
+    private List<FluxLimitation> thoracicLimitations = new LinkedList<FluxLimitation>();
 
     public enum Type { APNEA, HIPOAPNEA }
 
@@ -61,8 +60,8 @@ public class FluxLimitation extends TemporalEvent {
      */
     public String generateDescriptors(DETAILLEVEL level, long beginingRecording) {
         //@Emma que valores hay que poner si no hay ninguna limitacion toracica o abdominal??(duration == 0)
-        // ahora mismo no se añade nada a la string que se devuelve
-        String descriptors;
+        // ahora mismo no se añade nada al string que se devuelve
+
         //long duration;
         AbdominalMovementLimutation abdLim = new AbdominalMovementLimutation();
         ThoracicMovementLimutation thoraxLim = new ThoracicMovementLimutation();
@@ -70,73 +69,46 @@ public class FluxLimitation extends TemporalEvent {
         abdLim.setDuration(0);
         thoraxLim.setDuration(0);
 
-        descriptors = TimeRepresentation.timeToString(
-                                this.getAbsoluteBeginingTime(),false,true,false);
+        StringBuilder descriptors = new StringBuilder(TimeRepresentation.timeToString(
+                                this.getAbsoluteBeginingTime(),false,true,false));
 
         if(level == DETAILLEVEL.LOW){
-            if(this.getType() == Type.APNEA)    descriptors += "\tGEN_APNEA\t";
-            else                                descriptors += "\tGEN_HYPO\t";
-
-            descriptors += this.getDuration();
-        }
-        else{
-            descriptors +=   "\t" + (this.getAbsoluteBeginingTime()-beginingRecording)/1000F
-                     + "\t" + this.getDuration()/1000F;
-            if(this.getType() == Type.APNEA)    descriptors += "\tA";
-            else                                descriptors += "\tH";
+            descriptors.append("\tGEN_" + (this.getType() == Type.APNEA ? "APNEA" : "HYPO"));
+            descriptors.append("\t" + this.getDuration());
+        } else{
+            descriptors.append("\t" + (this.getAbsoluteBeginingTime() - beginingRecording)/1000F
+                     + "\t" + this.getDuration()/1000F);
+            descriptors.append("\t" + (this.getType() == Type.APNEA ? "A" : "H"));
 
             if(level == DETAILLEVEL.HIGH || level == DETAILLEVEL.EVERYTHING){
-                descriptors += "\t" + this.getEnergy() + "\t"
-                        + this.getBasalEnergyBefore() + "\t"
-                        + this.getBasalEnergyAfter();
+                descriptors.append("\t" + this.getEnergy()
+                        + "\t" + this.getBasalEnergyBefore()
+                        + "\t" + this.getBasalEnergyAfter());
                 if(level == DETAILLEVEL.EVERYTHING){
-                    descriptors = whenEverything(thoraxLim, abdLim, descriptors);
+                    descriptorWithEverything(thoraxLim, thoracicLimitations, descriptors);
+                    descriptorWithEverything(abdLim, abdominalLimitations, descriptors);
                 }
             }
         }
-        return descriptors;
+        return descriptors.toString();
     }
 
-    private String whenEverything(ThoracicMovementLimutation thoraxLim, AbdominalMovementLimutation abdLim, String descriptors){
-       //info de la limitacion toracica mas larga
-       for(ThoracicMovementLimutation lim: thoracicLimitations){
-           if(lim.getDuration()>thoraxLim.getDuration())
-               thoraxLim = lim;
+
+    private void descriptorWithEverything(FluxLimitation limit, List<FluxLimitation> limitations, StringBuilder descriptors){
+        FluxLimitation maxLim = limit;
+       for(FluxLimitation lim: limitations){
+           if(lim.getDuration() > maxLim.getDuration()){
+               maxLim = lim;
+           }
        }
-       duration = thoraxLim.getDuration();
+       duration = maxLim.getDuration();
        if(duration!=0){
-           descriptors += "\t" + thoraxLim.getDuration();
-
-           if(thoraxLim.getType() == Type.APNEA)
-               descriptors += "\tA\t";
-           else
-               descriptors += "\tH\t";
-
-           descriptors += thoraxLim.getEnergy() + "\t"
-                   + thoraxLim.getBasalEnergyBefore() + "\t"
-                   + thoraxLim.getBasalEnergyAfter();
+           descriptors.append("\t" + maxLim.getDuration());
+           descriptors.append("\t" + (maxLim.getType() == Type.APNEA ? "A" : "H"));
+           descriptors.append("\t" + maxLim.getEnergy()
+                   + "\t" + maxLim.getBasalEnergyBefore()
+                   + "\t" + maxLim.getBasalEnergyAfter());
        }
-
-       //info de la limitacion abdominal mas larga
-       for(AbdominalMovementLimutation lim : abdominalLimitations){
-           if(lim.getDuration()>abdLim.getDuration())
-               abdLim = lim;
-       }
-       duration = abdLim.getDuration();
-       if(duration!=0){
-           descriptors += "\t" + abdLim.getDuration();
-
-           if(abdLim.getType() == Type.APNEA)
-               descriptors += "\tA\t";
-           else
-               descriptors += "\tH\t";
-
-           descriptors += abdLim.getEnergy() + "\t"
-                   + abdLim.getBasalEnergyBefore() + "\t"
-                   + abdLim.getBasalEnergyAfter();
-       }
-       return descriptors;
-
     }
 
     public void addAbdomenLimitation(AbdominalMovementLimutation l) {
@@ -163,18 +135,12 @@ public class FluxLimitation extends TemporalEvent {
         return type;
     }
 
-    public List<AbdominalMovementLimutation> getAbdominalLimitations(){
-        List<AbdominalMovementLimutation> listCopy =
-            new LinkedList<AbdominalMovementLimutation>(abdominalLimitations);
-
-        return listCopy;
+    public List<FluxLimitation> getAbdominalLimitations(){
+        return new LinkedList<FluxLimitation>(abdominalLimitations);
     }
 
-    public List<ThoracicMovementLimutation> getThoracicLimitations(){
-        List<ThoracicMovementLimutation> listCopy =
-            new LinkedList<ThoracicMovementLimutation>(thoracicLimitations);
-
-        return listCopy;
+    public List<FluxLimitation> getThoracicLimitations(){
+        return new LinkedList<FluxLimitation>(thoracicLimitations);
     }
 
     public void setEnergy(float energy) {
@@ -193,5 +159,14 @@ public class FluxLimitation extends TemporalEvent {
         this.type = type;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
 
 }
